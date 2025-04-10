875bef14744d7c4cb8543086b02ca416523613f6
biku89 <biklukasz@gmail.com>
2025-04-10 21:26:43 +0200

    Ignore compiled output

--- Task1/src/Electronicts.java
+++
@@ -1,9 +0,0 @@
-public class Electronicts extends Product{
-    public Electronicts(int id, String name, double price, int quantityAvaliable) {
-        super(id, name, price, quantityAvaliable);
-    }
-    @Override
-    public void printInfo(){
-        super.printInfo();
-    }
-}
