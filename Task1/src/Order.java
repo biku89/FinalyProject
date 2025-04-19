f1ca776561b7cc6a134f2426faddd3872a36e4ff
biku89 <biklukasz@gmail.com>
2025-04-19 11:01:50 +0200

    +orderData

--- Task1/src/Order.java
+++ Task1/src/Order.java
@@ -1,15 +1,18 @@
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Order {
     private String clientName;
     private List<CartItem> orderItems;
     private BigDecimal priceSummary;
+    private LocalDateTime orderDate;
 
     public Order(String clientName, List<CartItem> orderItems) {
         this.clientName = clientName;
         this.orderItems = orderItems;
         this.priceSummary = sumOrder();
+        this.orderDate = LocalDateTime.now();
     }
 
     private BigDecimal sumOrder() {
@@ -19,6 +22,7 @@ public class Order {
 
     public void printOrderSummary() {
         System.out.println("Zamówienie dla: " + clientName);
+        System.out.println("Data: " + orderDate);
         System.out.println("Produkty: ");
         for (CartItem item : orderItems) {
             System.out.println(" - " + item.getProduct().getName()
